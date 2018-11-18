651868a110727876dd06aa2703e7c97661be8f42
Sindre Moldeklev <sindre.moldeklev@bekk.no>
2018-11-18 16:38:50 +0100

    Beskrivelse av vær-kommando lagt til

--- scripts/vaeret.js
+++ scripts/vaeret.js
@@ -1,3 +1,5 @@
-const {vaeret} = require('../dist/vaeret');
-    
-module.exports = vaeret
+// Commands:
+//    hubot vær - Returnerer været i Oslo for de neste fem dagene
+const { vaeret } = require('../dist/vaeret');
+
+module.exports = vaeret;
