f0b622b1f4258c6313b0dbbe277bb8239e0c344b
Sindre Moldeklev <sindre.moldeklev@bekk.no>
2018-12-16 20:26:59 +0100

    Dokumentert flere kommandoer

--- scripts/kolonial.js
+++ scripts/kolonial.js
@@ -1,6 +1,9 @@
 // Commands:
 //    hubot søk <produkt> - Returnerer produkter fra Kolonial.no basert på produktet du søker etter
 //    hubot opppskrifter <produkt> - Returnerer oppskrift fra Kolonial.no basert på produktet du søker etter
+//    hubot logg inn | login - Logger inn hos Kolonialen
+//    hubot logg ut | logout - Logger ut hos Kolonialen
+//    hubot handlekurv | hk - Viser handlekurven hos Kolonialen. Man må være logget inn for å vise denne
 const { kolonial } = require('../dist/kolonial');
 
 module.exports = kolonial;
